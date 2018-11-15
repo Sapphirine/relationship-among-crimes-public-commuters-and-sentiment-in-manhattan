28b67b3f80e073cd0362dd4b9208e6a526c5375b
Lin Bai <linbai023@gmail.com>
2018-11-14 22:41:11 -0500

    sent1

--- relationship-between-population-flow-and-criminal-rate-in-manhattan/src/components/twitter_sentiment/index.vue
+++ relationship-between-population-flow-and-criminal-rate-in-manhattan/src/components/twitter_sentiment/index.vue
@@ -1,11 +1,40 @@
 <template>
+  <div>
+    <h1>Twitter Sentiment</h1>
+    Current Value  {{value}}
     <div>
-        <h1>Twitter Sentiment</h1>
+      <img :src="img" alt="img">
     </div>
+    <div>
+      <button @click="increase">Click</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    
+export default {
+  components: {
+  },
+  name: 'twitter',
+  data: function() {
+    return {
+      value: 0,
+      img: undefined,
+      imgs: [
+        "static/images/1.png",
+        "static/images/2.png",
+        "static/images/3.png",
+        "static/images/4.png",
+      ],
+    }
+  },
+  methods:{
+    increase:function(){
+      this.value ++;
+      this.img = this.imgs[this.value%4]
+    }
+  }
+}
 </script>
 
 <style>
