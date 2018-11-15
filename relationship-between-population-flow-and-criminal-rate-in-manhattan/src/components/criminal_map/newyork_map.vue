f93e2092fd0f4414262427bae1daeffdf2dd3814
Lin Bai <linbai023@gmail.com>
2018-11-14 20:06:25 -0500

    update coordinates

--- relationship-between-population-flow-and-criminal-rate-in-manhattan/src/components/criminal_map/newyork_map.vue
+++ relationship-between-population-flow-and-criminal-rate-in-manhattan/src/components/criminal_map/newyork_map.vue
@@ -61,7 +61,72 @@ export default {
               geoCenter[74]["center"]["coordinates"][0] = -74.106616
               geoCenter[74]["center"]["coordinates"][1] = 40.588817
 
-               40.720382
+              geoCenter[73]["center"]["coordinates"][0] = -74.077465
+              geoCenter[73]["center"]["coordinates"][1] = 40.644822
+
+              
+              geoCenter[8]["center"]["coordinates"][0] = -73.970820
+              geoCenter[8]["center"]["coordinates"][1] = 40.756937
+
+              geoCenter[15]["center"]["coordinates"][0] = -73.941132
+              geoCenter[15]["center"]["coordinates"][1] = 40.801098
+
+              geoCenter[22]["center"]["coordinates"][0] = -73.925368
+              geoCenter[22]["center"]["coordinates"][1] = 40.810569
+
+              geoCenter[23]["center"]["coordinates"][0] = -73.895629
+              geoCenter[23]["center"]["coordinates"][1] = 40.816424
+
+              geoCenter[27]["center"]["coordinates"][0] = -73.827296
+              geoCenter[27]["center"]["coordinates"][1] = 40.83107
+
+              geoCenter[29]["center"]["coordinates"][0] = -73.847437
+              geoCenter[29]["center"]["coordinates"][1] = 40.887754
+
+              geoCenter[30]["center"]["coordinates"][0] = -73.900396
+              geoCenter[30]["center"]["coordinates"][1] = 40.844155
+
+              geoCenter[33]["center"]["coordinates"][0] = -73.879678
+              geoCenter[33]["center"]["coordinates"][1] = 40.869286
+
+              geoCenter[37]["center"]["coordinates"][0] = -73.941588
+              geoCenter[37]["center"]["coordinates"][1] = 40.628193
+
+              geoCenter[41]["center"]["coordinates"][0] = -73.905097
+              geoCenter[41]["center"]["coordinates"][1] = 40.64882
+
+              geoCenter[46]["center"]["coordinates"][0] = -73.881488
+              geoCenter[46]["center"]["coordinates"][1] = 40.671378
+
+              geoCenter[51]["center"]["coordinates"][0] = -73.924286
+              geoCenter[51]["center"]["coordinates"][1] = 40.689894
+
+              geoCenter[54]["center"]["coordinates"][0] = -73.960528
+              geoCenter[54]["center"]["coordinates"][1] = 40.690305
+
+              geoCenter[57]["center"]["coordinates"][0] = -73.816488
+              geoCenter[57]["center"]["coordinates"][1] = 40.586525
+
+              geoCenter[63]["center"]["coordinates"][0] = -73.839768
+              geoCenter[63]["center"]["coordinates"][1] = 40.682496
+
+              geoCenter[70]["center"]["coordinates"][0] = -73.775763
+              geoCenter[70]["center"]["coordinates"][1] = 40.679988
+
+              geoCenter[71]["center"]["coordinates"][0] = -73.915272
+              geoCenter[71]["center"]["coordinates"][1] = 40.769531
+
+
+
+              
+
+
+              for(var i = 0; i < geoCenter.length; i++){
+                if(geoCenter[i]["center"]["coordinates"][0] == 0){
+                  console.log(i, geoCenter[i]["precinct"])
+                }
+              }
+              
                console.log(geoCenter)
                return geoCenter;
             })
@@ -89,6 +154,7 @@ export default {
   }
   // TODO: fire events
 }
+
 </script>
 
 <style>
